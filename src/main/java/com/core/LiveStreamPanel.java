5b1045a3bcd5bab316a0bcbc57692aac28ff0815
William Swinny <williamwswinny@gmail.com>
2016-03-19 22:36:10 -0500

    Fixed missing src folder

---
+++ src/main/java/com/core/LiveStreamPanel.java
@@ -0,0 +1,14 @@
+package com.core;
+
+import javax.swing.*;
+
+/**
+ * Created by Swinny on 3/19/2016.
+ */
+public class LiveStreamPanel extends JPanel
+{
+    public LiveStreamPanel()
+    {
+
+    }
+}
