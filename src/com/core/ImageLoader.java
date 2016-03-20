aee406f606083e71b73b7834d875097ce434f8c1
William Swinny <williamwswinny@gmail.com>
2016-03-19 22:24:46 -0500

    Added blank classes and foundation ideas

---
+++ src/com/core/ImageLoader.java
@@ -0,0 +1,7 @@
+package com.core;
+
+/**
+ * Created by Swinny on 3/19/2016.
+ */
+public class ImageLoader {
+}
---
+++ src/com/core/Main.java
@@ -0,0 +1,19 @@
+package com.core;
+
+/**
+ * Created by Swinny on 3/19/2016.
+ */
+public class Main
+{
+    public static String STORAGE_DIR = "path to images";
+
+    public Main()
+    {
+
+    }
+
+    public static void main(String [] args)
+    {
+        new Main();
+    }
+}
