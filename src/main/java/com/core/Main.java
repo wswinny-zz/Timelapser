3b672da0d74360efccd423f611ad9337f346fc51
William Swinny <williamwswinny@gmail.com>
2016-03-19 22:38:53 -0500

    Added OpenCV

--- src/main/java/com/core/Main.java
+++ src/main/java/com/core/Main.java
@@ -1,10 +1,13 @@
 package com.core;
 
+import org.opencv.core.Core;
+
 /**
  * Created by Swinny on 3/19/2016.
  */
 public class Main
 {
+    static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
     public static String STORAGE_DIR = "path to images";
 
     public Main()
