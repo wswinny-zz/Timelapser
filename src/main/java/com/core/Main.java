36f013048fe3bb62be0cb30d32b661447ba692d3
William Swinny <williamwswinny@gmail.com>
2016-03-23 00:56:54 -0500

    Added arrow key functionality and added space bar for switching panels

--- src/main/java/com/core/Main.java
+++ src/main/java/com/core/Main.java
@@ -1,17 +1,17 @@
 package com.core;
 
-import javafx.scene.control.Slider;
 import org.opencv.core.Core;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 
 /**
  * Created by Swinny on 3/19/2016.
  */
-public class Main
+public class Main implements KeyListener
 {
     static
     {
@@ -20,16 +20,19 @@ public class Main
         System.load(opencvpath + "opencv_ffmpeg300_64.dll");
     }
 
-    public static final String STORAGE_DIR = "C:\\Users\\Saphixx\\Desktop\\TimeTest\\";
+    public static final String STORAGE_DIR = "D:\\Users\\Swinny\\Desktop\\test\\";
     public static final String VIDEO_SOURCE = "http://217.91.58.189:1024/mjpg/video.mjpg";
 
     public static final int IMAGE_SAVE_RATE = 1000; //in miliseconds
 
     private JFrame frame;
-    private SliderPanel sliderPanel;
-    private LiveStreamPanel liveStreamPanel;
+    private JPanel sliderPanel;
+    private JSlider timeSlider;
 
-    private int totalImageCount = 0;
+    private JLayeredPane layeredPane;
+
+    private JPanel picturePanel;
+    private LiveStreamPanel liveStreamPanel;
 
     public Main()
     {
@@ -40,21 +43,52 @@ public class Main
             System.exit(69);
         }
 
-        this.totalImageCount = imageDir.listFiles().length;
-
         this.frame = new JFrame();
-        //this.frame.setUndecorated(true);
+//        this.frame.setUndecorated(true);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //this.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        this.frame.setSize(new Dimension(800, 600));
+//        this.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.frame.setSize(new Dimension(1600, 900));
         this.frame.setLocationRelativeTo(null);
         this.frame.getContentPane().setBackground(Color.DARK_GRAY);
 
-        this.liveStreamPanel = new LiveStreamPanel();
-        this.frame.add(this.liveStreamPanel, BorderLayout.CENTER);
+        this.layeredPane = new JLayeredPane();
+        this.frame.add(this.layeredPane, BorderLayout.CENTER);
 
-        this.sliderPanel = new SliderPanel();
+        this.picturePanel = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g)
+            {
+                g.drawImage(ImageLoader.getInstance().getcurrentImage(), 0, 0, getWidth(), getHeight(), null);
+            }
+        };
+
+        this.picturePanel.setBackground(Color.DARK_GRAY);
+        this.picturePanel.setBounds(0, 0, 1600, 850);
+        this.layeredPane.add(this.picturePanel, JLayeredPane.PALETTE_LAYER);
+
+        this.liveStreamPanel = new LiveStreamPanel();
+        this.liveStreamPanel.setBounds(0, 0, 1600, 850);
+        this.layeredPane.add(this.liveStreamPanel, JLayeredPane.DEFAULT_LAYER);
+
+        this.sliderPanel = new JPanel();
+        this.sliderPanel.setLayout(new GridLayout(1, 2));
+        this.sliderPanel.setBackground(Color.DARK_GRAY);
         this.frame.add(this.sliderPanel, BorderLayout.SOUTH);
+
+        this.timeSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 0);
+        this.timeSlider.setMinorTickSpacing(1);
+        this.timeSlider.setMajorTickSpacing(5);
+        this.timeSlider.setPaintTicks(false);
+        this.timeSlider.setBackground(Color.DARK_GRAY);
+
+        this.sliderPanel.add(this.timeSlider);
+
+        this.frame.addKeyListener(this);
+        this.liveStreamPanel.addKeyListener(this);
+        this.sliderPanel.addKeyListener(this);
+        this.timeSlider.addKeyListener(this);
+        this.layeredPane.addKeyListener(this);
+        this.picturePanel.addKeyListener(this);
 
         this.frame.setVisible(true);
     }
@@ -72,4 +106,36 @@ public class Main
 
         new Main();
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if(e.getKeyChar() == ' ') {
+            swapPanels();
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            ImageLoader.getInstance().getNextImage();
+            this.picturePanel.repaint();
+        }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+            ImageLoader.getInstance().getPrevImage();
+            this.picturePanel.repaint();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    private void swapPanels()
+    {
+        int temp = this.layeredPane.getLayer(this.liveStreamPanel);
+        this.layeredPane.setLayer(this.liveStreamPanel, this.layeredPane.getLayer(this.picturePanel));
+        this.layeredPane.setLayer(this.picturePanel, temp);
+    }
+
 }
